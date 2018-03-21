206a5dfba13b6ca39cf7d371ae2903be580c9078
p_val <palval16@gmail.com>
2018-03-21 10:51:43 +0100

    12 [not finished]

--- src/t12.kt
+++ src/t12.kt
@@ -6,5 +6,44 @@
 
 
 fun main(args: Array<String>) {
+//    var i = 1
+//    while (factorsNumber(i).size < 500) {
+//        i++
+//        println(i)
+//    }
+    println(factorsNumber(100000).size)
+}
 
+// https://www.mathsisfun.com/numbers/factors-all-tool.html
+fun factorsNumber(number: Int): Set<Int> {
+    val smaller = arrayListOf(1)
+    val bigger = arrayListOf(number)
+    var nextSmall = smaller.last()
+    while (bigger.last() - smaller.last() > 1) {
+        nextSmall++
+        if (nextSmall >= bigger.first()) {
+            break
+        }
+        for (nextBig in (bigger.first() - 1) downTo nextSmall) {
+            if (nextBig < 0) {
+                return smaller.plus(bigger.reversed()).toSet()
+            }
+            if (nextSmall * nextBig == number) {
+                smaller.add(nextSmall)
+                bigger.add(nextBig)
+                break
+            } else {
+                continue
+            }
+        }
+    }
+    return smaller.plus(bigger.reversed()).toSet()
+}
+
+fun triangleNumberFrom(i: Int): Int {
+    var acum = 0
+    for (n in 0..i) {
+        acum += n
+    }
+    return acum
 }
