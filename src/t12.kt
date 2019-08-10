b8c0f2324bdb02d7b52765953ed7ee72e9bebbd8
p_val <palval16@gmail.com>
2019-08-10 18:08:03 +0200

    Solved 12

--- src/t12.kt
+++ src/t12.kt
@@ -1,49 +1,9 @@
-/*
- * @author Valeriy Palamarchuk
- * @email valeriij.palamarchuk@gmail.com
- * Created on 3/18/18
- */
-
-
-fun main(args: Array<String>) {
-//    var i = 1
-//    while (factorsNumber(i).size < 500) {
-//        i++
-//        println(i)
-//    }
-    println(factorsNumber(28).size)
-}
-
-// https://www.mathsisfun.com/numbers/factors-all-tool.html
-fun factorsNumber(number: Int): Set<Int> {
-    val smaller = arrayListOf(1)
-    val bigger = arrayListOf(number)
-    var nextSmall = smaller.last()
-    while (bigger.last() - smaller.last() > 1) {
-        nextSmall++
-        if (nextSmall >= bigger.first()) {
-            break
-        }
-        for (nextBig in (bigger.first() - 1) downTo nextSmall) {
-            if (nextBig < 0) {
-                return smaller.plus(bigger.reversed()).toSet()
-            }
-            if (nextSmall * nextBig == number) {
-                smaller.add(nextSmall)
-                bigger.add(nextBig)
-                break
-            } else {
-                continue
-            }
-        }
+fun main() {
+    var acc = 0
+    var i = 1
+    while ((1..acc).count { acc % it == 0 } < 500) {
+        acc += i
+        i++
     }
-    return smaller.plus(bigger.reversed()).toSet()
-}
-
-fun triangleNumberFrom(i: Int): Int {
-    var acum = 0
-    for (n in 0..i) {
-        acum += n
-    }
-    return acum
+    println(acc)
 }
---
+++ src/t14.kt
@@ -0,0 +1,8 @@
+/*
+ * @author Valeriy Palamarchuk
+ * @email valeriij.palamarchuk@gmail.com
+ * Created on 2019-08-10
+ */
+fun main() {
+
+}
